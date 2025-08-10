09b831dcccebaa72f325e30d68f2213a0944b374
Hawa Awalya G <hawaawalyaghufron@gmail.com>
2025-08-11 05:26:21 +0700

    Update datacleaningretail.sql
    add eda

--- datacleaningretail.sql
+++ datacleaningretail.sql
@@ -1,4 +1,4 @@
-
+-- DATA CLEANING PROSES
 DROP TABLE IF EXISTS online_retail_clean;
 
 CREATE TABLE online_retail_clean AS
@@ -51,6 +51,61 @@ OR country IS NULL
 OR TRIM(country) = '';
 
 SELECT DISTINCT country FROM online_retail_clean ORDER BY country;
+
+--EDA
+SELECT 
+    COUNT(*) AS total_transaksi,
+    MIN(invoicedate) AS tanggal_pertama,
+    MAX(invoicedate) AS tanggal_terakhir
+FROM online_retail_clean;
+
+SELECT 
+    country,
+    COUNT(DISTINCT invoiceno) AS total_order,
+    SUM(totalsales) AS total_revenue
+FROM online_retail_clean
+GROUP BY country
+ORDER BY total_revenue DESC;
+
+--Produk terlaris (berdasarkan quantity)
+SELECT 
+    stockcode,
+    description,
+    SUM(quantity) AS total_qty
+FROM online_retail_clean
+GROUP BY stockcode, description
+ORDER BY total_qty DESC
+LIMIT 10;
+
+--Produk dengan revenue tertinggi
+SELECT 
+    stockcode,
+    description,
+    SUM(totalsales) AS revenue
+FROM online_retail_clean
+GROUP BY stockcode, description
+ORDER BY revenue DESC
+LIMIT 10;
+
+--Tren penjualan per bulan
+SELECT 
+    DATE_TRUNC('month', invoicedate) AS bulan,
+    SUM(totalsales) AS revenue
+FROM online_retail_clean
+GROUP BY DATE_TRUNC('month', invoicedate)
+ORDER BY bulan;
+
+--Top customer berdasarkan revenue
+SELECT 
+    customerid,
+    SUM(totalsales) AS revenue,
+    COUNT(DISTINCT invoiceno) AS jumlah_order
+FROM online_retail_clean
+GROUP BY customerid
+ORDER BY revenue DESC
+LIMIT 10;
+
+
 
 
 
